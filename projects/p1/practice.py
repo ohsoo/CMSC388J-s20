19b9d67f08bec3445c8ecdda9b8268c19d6a42af
SOO OH <soooh@SOOs-MBP.fios-router.home>
2020-02-12 13:04:53 -0500

    p1 done

--- projects/p1/practice.py
+++ projects/p1/practice.py
@@ -1,4 +1,3 @@
-
 def hello_world():
     """ Returns 'Hello, World!'
 
@@ -9,7 +8,7 @@ def hello_world():
     >>> hello_world()
     'Hello, World!'
     """
-    pass
+    return 'Hello, World!'
 
 
 def sum_unique(l):
@@ -28,7 +27,17 @@ def sum_unique(l):
     >>> sum_unique([2, 2, 2, 2, 1])
     3
     """
-    pass
+    n = len(l)
+    if (n == 0):
+        return 0
+
+    l.sort()
+    sum = l[0]
+    for i in range(n-1):
+        if (l[i] != l[i+1]):
+            sum += l[i+1]
+
+    return sum
 
 def palindrome(x):
     """ Determines if x, an integer or string, is a palindrome, i.e.
@@ -47,7 +56,17 @@ def palindrome(x):
     >>> palindrome('python')
     False
     """
-    pass
+    l = [];
+    if (type(x) == str):
+        l = [char for char in x]
+    else:
+        l = [int(x) for x in str(x)]
+
+    rev = l[::-1]
+    for i in range(len(l)):
+        if (l[i] != rev[i]):
+            return False
+    return True
 
 def sum_multiples(num):
     """ Sums up all multiples of 3 and 5 upto and not including num.
@@ -65,7 +84,12 @@ def sum_multiples(num):
     >>> sum_multiples(16) # Multiples: [3, 5, 6, 9, 10, 12, 15]
     60
     """
-    pass
+    sum = 0
+    if (num > 3):
+        for i in range(3, num):
+            if (i % 3 == 0 or i % 5 == 0):
+                sum += i
+    return sum
 
 def num_func_mapper(nums, funs):
     """ Applies each function in funs to the list of numbers, nums, and
@@ -82,7 +106,11 @@ def num_func_mapper(nums, funs):
     >>> num_func_mapper(num_list, f_list)
     [11, 15]
     """
-    pass
+    l = []
+    for fn in funs:
+        num = fn(nums)
+        l.append(num)
+    return l
 
 def pythagorean_triples(n):
     """ Finds all pythagorean triples where a, b, and c (sides of the triangle)
@@ -104,4 +132,12 @@ def pythagorean_triples(n):
     >>> pythagorean_triples(20)
     [(3, 4, 5), (6, 8, 10), (5, 12, 13), (9, 12, 15), (8, 15, 17)]
     """
-    pass
+    l = []
+    if (n > 3):
+        for c in range(5, n):
+            for b in range(4, c):
+                for a in range(3, b):
+                    if (a*a + b*b == c*c):
+                        l.append((a,b,c))
+                        break
+    return l
